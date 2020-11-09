ee570ff1d50f748ecd646158aebe193a7e6ff90f
Ryan Mitchell <ryan@thoughtcollective.com>
2020-11-09 10:20:31 +0000

    Handle multiple printers

--- source/app/containers/HomePage.tsx
+++ source/app/containers/HomePage.tsx
@@ -15,216 +15,286 @@ const nc2 = new NetcatClient();
 import styles from './Home.module.css';
 
 export default function App() {
-	
-	const [serverProcess, setServerProcess] = useState(null);
-	const [isProcessRunning, setProcessRunning] = useState(false);
+
+	const [serverProcess, setServerProcess] = useState({});
 	const [logData, setLogData] = useState([]);
-	
+	const [printerList, setPrinterList] = useState([
+		{listenport: '9001', ipaddress: '192.168.0.1', port: '9100',},
+    ]);
 	const [formValues, setFormValues] = useState({
-		listenport: '9001',
-		ipaddress: '192.168.0.1',
-		port: '9100',
 		cert: '',
 		key: ''
 	});
-	
+
 	useEffect(() => {
-	
+
 		AsyncStorage.getItem('@Printer:savedValues')
 		.then(savedValues => {
 			if (savedValues !== null) {
 				setFormValues(JSON.parse(savedValues));
 			}
-		});	
+		});
+
+		AsyncStorage.getItem('@Printer:printerList')
+		.then(savedValues => {
+			if (savedValues !== null) {
+				setPrinterList(JSON.parse(savedValues));
+			}
+		});
 
 	}, []);
-	
-	function _startServer(){
 
-		_log('Starting server');
-		
+	function _startServer(idx){
+
+		_log('Starting printer ' + idx);
+
 		try {
 
 			let serverOptions = {};
-			
+
 			if (formValues.cert && formValues.cert != ''){
 				serverOptions = {
 					cert: formValues.cert.split('\r'),
 					key: formValues.key.split('\r')
 				};
 			}
-						
+
 			const server = serverOptions.cert ? https.createServer(serverOptions) : http.createServer();
-						
+
 			const wss = new WebSocket.Server({ server });
-						
+
 			let client;
-			
+
 			wss.on('listening', function(){
-				_log('listening for connections on port ' + formValues.listenport);			
+				_log('printer ' + idx + ' listening for connections on port ' + printerList[idx].listenport);
 			});
-			
+
 			wss.on('error', function(error){
 				_log(error.message);
 			});
-			
+
 			wss.on('connection', function connection(ws){
-				
-				_log('Connection');
-				
+
+				_log('Connection to printer ' + idx);
+
 			  	ws.on('message', function incoming(message){
 				  	_log('Printing');
-				  		
-				  	// removed this intentionally - send the buffer not a string			  	
+
+				  	// removed this intentionally - send the buffer not a string
 				  	//message = message.toString();
-				  					  	
+
 				  	nc2
-				  	.addr(formValues.ipaddress)
-				  	.port(parseInt(formValues.port))
+				  	.addr(printerList[idx].ipaddress)
+				  	.port(parseInt(printerList[idx].port))
 				  	.tcp()
 				  	.connect()
 				  	.end(message);
 
 			  	});
-			  
+
 			});
-			
-			server.listen(formValues.listenport);
-			
-			setServerProcess(server);
-		
+
+			server.listen(printerList[idx].listenport);
+
+            serverProcess[idx] = server;
+			setServerProcess(serverProcess);
+
 		} catch (e){
 			_log(e.message);
 		}
-				
-		setProcessRunning(true);
-		
-		AsyncStorage.setItem('@Printer:savedValues', JSON.stringify(formValues));
-				
+
 	}
-	
-	function _stopServer(){
-		
-		if (serverProcess){
+
+	function _stopServer(idx){
+
+		if (serverProcess[idx]){
 			//serverProcess.kill('SIGINT');
-			serverProcess.close();
+			serverProcess[idx].close();
+            delete serverProcess[idx];
+			setServerProcess(serverProcess);
 		}
-		
-		setProcessRunning(false);
-		_log('Stopping proxy server');
-		
+
+		_log('Stopping printer ' + idx);
+
 	}
-	
+
+	function _addPrinter(){
+        let newPrinterList = [];
+        for (let i=0;i<printerList.length;i++){
+            newPrinterList.push(printerList[i]);
+        }
+        newPrinterList.push({listenport: '9001', ipaddress: '192.168.0.1', port: '9100',});
+        setPrinterList(newPrinterList);
+	}
+
+	function _removePrinter(idx){
+        let newPrinterList = [];
+        for (let i=0;i<printerList.length;i++){
+            if (i != idx){
+                newPrinterList.push(printerList[i]);
+            } else {
+                _stopServer(idx);
+            }
+        }
+        setPrinterList(newPrinterList);
+	}
+
 	function _log(msg){
 		logData.push('[' + new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }) + ']: ' + msg);
 		setLogData([...logData]);
 	}
-	
+
 	function _downloadCert(){
-		
+
 		fs.readFile(path.join(__dirname, './certs/localhost.crt'), 'utf8', function(err, contents) {
 		    clipboard.writeText(contents, 'selection')
 		});
-	
+
 	}
-	
+
+	function handlePrinter(idx, key, event){
+        let newPrinterList = [];
+        for (let i=0;i<printerList.length;i++){
+            newPrinterList.push(printerList[i]);
+        }
+		newPrinterList[idx][key] = event.target.value;
+		setPrinterList(newPrinterList);
+		AsyncStorage.setItem('@Printer:printerList', JSON.stringify(newPrinterList));
+	}
+
 	function handleChange(key, event){
 		formValues[key] = event.target.value;
 		setFormValues({...formValues});
+		AsyncStorage.setItem('@Printer:savedValues', JSON.stringify(formValues));
 	}
-					
+
+    let outputPrinters = [];
+    function run_outputPrinters(){
+
+        outputPrinters = [];
+
+        printerList.forEach(function(printer, idx){
+        outputPrinters.push((
+            <tr style={{ height: 40 }}>
+                  <td width="10%">
+        				<a onClick={_removePrinter.bind(this, idx)} className={styles.buttonRemove}>X</a>
+                  </td>
+                  <td width="20%">
+        				<input
+        					onChange={text => handlePrinter(idx, 'listenport', text)}
+        					value={printer.listenport}
+        			        autoCapitalize='none'
+        			        className={styles.textInput}
+                            style={{ width: 90 }}
+        				/>
+                  </td>
+                  <td width="30%">
+        				<input
+        					onChange={text => handlePrinter(idx, 'ipaddress', text)}
+        					value={printer.ipaddress}
+        			        autoCapitalize='none'
+        			        className={styles.textInput}
+        				/>
+                  </td>
+                  <td width="20%">
+        				<input
+        					onChange={text => handlePrinter(idx, 'port', text)}
+        					value={printer.port}
+        			        autoCapitalize='none'
+        			        className={styles.textInput}
+                            style={{ width: 90 }}
+        				/>
+                  </td>
+                  <td width="20%" style={{ textAlign: 'right' }}>
+        				{!serverProcess[idx] &&
+        				  <a onClick={_startServer.bind(this, idx)} className={styles.buttonStart}>Start</a>
+        				}
+        				{serverProcess[idx] &&
+        				  <a onClick={_stopServer.bind(this, idx)} className={styles.buttonStop}>
+        				  	Stop
+        				  </a>
+        				}
+                  </td>
+                </tr>
+              )
+            );
+        }, this);
+
+    }
+
+    run_outputPrinters();
+
 	return (
 	<div className={styles.container}>
-	
-		<div style={{ width: '100%', height: 340, display: 'flex', flexDirection: 'row', backgroundColor: '#ccc' }}>
-				
-			<div style={{ flex: 1, padding: 20 }}>
-			
-				<p style={{ fontSize: 24, marginBottom: 10 }}>Setup</p>
-			
-				<p className={styles.textInputLabel}>Listen on port</p>
-			
-				<input
-					onChange={text => handleChange('listenport', text)}
-					value={formValues.listenport}
-			        autoCapitalize='none'
-			        className={styles.textInput}
-				/>
-				
-				<p className={styles.textInputLabel}>Printer IP address</p>
-			
-				<input
-					onChange={text => handleChange('ipaddress', text)}
-					value={formValues.ipaddress}
-			        autoCapitalize='none'
-			        className={styles.textInput}
-				/>
-				
-				<p className={styles.textInputLabel}>Printer Port</p>
-			
-				<input
-					onChange={text => handleChange('port', text)}
-					value={formValues.port}
-			        autoCapitalize='none'
-			        className={styles.textInput}
-				/>
-				
-			</div>
-			
-			<div style={{ flex: 1, padding: 20, paddingLeft: 0 }}>
-			
-				<p style={{ fontSize: 24, marginBottom: 10 }}>&nbsp;</p>
-				
-				<p className={styles.textInputLabel}>SSL cert</p>
-			
-				<textarea
-					onChange={text => handleChange('cert', text)}
-					value={formValues.cert}
-			        autoCapitalize='none'
-			        className={styles.textareaInput}
-				/>
 
-				<p className={styles.textInputLabel}>SSL key</p>
-			
-				<textarea
-					onChange={text => handleChange('key', text)}
-					value={formValues.key}
-			        autoCapitalize='none'
-			        className={styles.textareaInput}
-				/>
-				
-				<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-				
-					<div style={{ marginTop: 25, width: 160 }}>
-							
-						{isProcessRunning == false && 
-						  <a onClick={_startServer.bind(this)} className={styles.button}>Start print proxy</a>
-						}
-						{isProcessRunning && 
-						  <a onClick={_stopServer.bind(this)} className={styles.button}>
-						  	Stop print proxy
-						  </a>
-						}
-						
-					</div>
-				
-				</div>
-						
+		<div style={{ width: '100%', height: 360, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+
+  	      <p style={{ fontSize: 24, marginBottom: 10 }}>Setup</p>
+
+		  <div style={{ width: '100%', display: 'flex', flexDirection: 'row', backgroundColor: '#ccc' }}>
+
+  			<div style={{ padding: 20, paddingTop: 10 }}>
+
+  				<p className={styles.textInputLabel}>SSL cert</p>
+
+  				<textarea
+  					onChange={text => handleChange('cert', text)}
+  					value={formValues.cert}
+  			        autoCapitalize='none'
+  			        className={styles.textareaInput}
+  				/>
+
+            </div>
+
+  		    <div style={{ padding: 20, paddingTop: 10, paddingLeft: 0, marginLeft: -15 }}>
+
+  				<p className={styles.textInputLabel}>SSL key</p>
+
+  				<textarea
+  					onChange={text => handleChange('key', text)}
+  					value={formValues.key}
+  			        autoCapitalize='none'
+  			        className={styles.textareaInput}
+  				/>
+
+            </div>
+
+        </div>
+
+		<div style={{ flex: 1, paddingTop: 20, }}>
+
+        <table width="100%">
+          <tr>
+            <th> </th>
+            <th className={styles.textInputLabel}>Listen port</th>
+            <th className={styles.textInputLabel}>Printer IP</th>
+            <th className={styles.textInputLabel}>Printer port</th>
+            <th> </th>
+          </tr>
+          <tbody id="printerList">
+            {outputPrinters}
+          </tbody>
+        </table>
+
+        <div style={{ marginTop: 20 }}>
+    		  <a onClick={_addPrinter.bind(this)} className={styles.buttonAdd}>Add printer</a>
+        </div>
+
 			</div>
-		
+
 		</div>
-		
-		<div style={{ width: '100%', flex: 1, padding: 20 }}>
-		
+
+		<div style={{ width: '100%', flex: 1, paddingTop: 20 }}>
+
 			<p style={{ fontSize: 24, marginBottom: 10 }}>Logs</p>
-			
-			<div style={{ fontSize: 12, borderWidth: 1, borderStyle:'solid', borderColor: '#ccc', overflow: 'scroll', height: 120, marginLeft: -20, boxSizing:'border-box', width: '100%' }}>
+
+			<div style={{ fontSize: 12, borderWidth: 1, borderStyle:'solid', borderColor: '#ccc', overflow: 'scroll', height: 120, boxSizing:'border-box', width: '100%' }}>
 				<pre style={{ padding: 0, margin: 0 }}>{[...logData].reverse().join("\n")}</pre>
 			</div>
-		
+
 		</div>
-	
+
 	</div>
 	);
-  
+
 }
